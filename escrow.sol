780d8841100a62be54b93ed14a50a8575dbb4afe
Manuel InfoSec <manuelinfosec@gmail.com>
2022-10-03 03:36:54 +0100

    Local Compiler v0.7.0

--- escrow.sol
+++ escrow.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.7.0;
 
 // SPDX-License-Identifier: MIT
-import "https://raw.githubusercontent.com/smartcontractkit/chainlink/develop/evm-contracts/src/v0.6/ChainlinkClient.sol";
+import "@smartcontractkit/chainlink/develop/contracts/src/v0.6/ChainlinkClient.sol";
 
 contract Escrow is ChainlinkClient {
   
